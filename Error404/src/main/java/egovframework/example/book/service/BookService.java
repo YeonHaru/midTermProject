fde8d156136c4cd8540c3a91fb3644d8436e769e
YeonHaru <pigson98@gmail.com>
2025-07-03 14:13:59 +0900

    11
    11

---
+++ Error404/src/main/java/egovframework/example/book/service/BookService.java
@@ -0,0 +1,10 @@
+package egovframework.example.book.service;
+
+import java.util.List;
+
+import egovframework.example.common.Criteria;
+
+public interface BookService {
+	List<?> selectBookList(Criteria criteria);
+	BookVO selectBook(int bno);
+}
