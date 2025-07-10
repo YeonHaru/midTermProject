0991e6fea5ddd057007b1ac1b3928d4481cba7ac
karch1 <rkdeotjdak@naver.com>
2025-07-10 17:06:56 +0900

    강대성
    7월10일 오후

--- Error404/src/main/java/egovframework/example/users/service/UsersVO.java
+++ Error404/src/main/java/egovframework/example/users/service/UsersVO.java
@@ -32,13 +32,14 @@ public class UsersVO extends Criteria{
 	    private String email;
 	    private String phone;
 	    private LocalDate join_date;    // DATE → LocalDate
-	    private String role = "GENERAL" ; // 일반 등급 기본값 : 덕규(7/10)
+	    private String role; 			// 일반 등급 기본값 : 덕규(7/10)
 	    private String gender;
 	    private String birthdate;    
 	    private String address;
 	    private Integer point;          // null 가능성을 고려해 Integer 사용
 	    private String promoAgree;		// 알림수신동의
 	    private String postNotifyAgree;	// 알림수신동의
-	    private String grade;			// 회원등급
+	    private String grade = "GENERAL"; // 회원등급
 	    private String tempPwYn; 		// 임시 비밀번호 여부 ('Y' or 'N')
+	    private Integer totalPurchase;  // 총 누적 구매 금액
 }
