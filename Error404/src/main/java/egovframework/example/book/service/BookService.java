a3949b5fdab26541433a18d63ca4e78864cb0d01
dalmaru-86 <tlstmdghk2@naver.com>
2025-07-14 14:23:42 +0900

    신승화
    메인페이지 베스트 셀러 관련한 수정,
    북컨트롤러 제외한 북관련 VO 서비스등 코딩

--- Error404/src/main/java/egovframework/example/book/service/BookService.java
+++ Error404/src/main/java/egovframework/example/book/service/BookService.java
@@ -8,15 +8,11 @@ public interface BookService {
 	List<?> selectBookList(Criteria criteria);
 	BookVO selectBook(int bno);
 	List<BookVO> searchBookList(Criteria criteria);
-	List<BookVO> selectLatestBooks();  // 메인페이지 신간 도서 4개용(승화)
-	List<BookVO> selectRandomBooks();  // 메인페이지 랜덤 도서 4개용(승화)
-
-    int selectBookListTotCnt(Criteria criteria); 	    // 총 개수 구하기
-
+	List<BookVO> selectLatestBooks();  				    	// 메인페이지 신간 도서 4개용(승화)
+	List<BookVO> selectRandomBooks(); 				   	    // 메인페이지 랜덤 도서 4개용(승화)
+    int selectBookListTotCnt(Criteria criteria); 	        // 총 개수 구하기
 	List<BookVO> selectBooksByBnoList(List<Integer> bnoList);  // 최근 본 상품(덕규)
-	
-
-
-	List<BookVO> selectFlipBooks();	   // 메인페이지 할인율 플립 도서 4개용(승화)
-	BookVO getRandomOnSaleBook();	// 랜덤도서 1권 가져오는용(덕규)
+	List<BookVO> selectFlipBooks();	  					    // 메인페이지 할인율 플립 도서 4개용(승화)
+	List<BookVO> selectBestSellerList();			    	// 메인페이지 베스트셀러(승화)
+	BookVO getRandomOnSaleBook();					    	// 랜덤도서 1권 가져오는용(덕규)
 }
--- Error404/src/main/java/egovframework/example/book/service/BookVO.java
+++ Error404/src/main/java/egovframework/example/book/service/BookVO.java
@@ -27,9 +27,14 @@ public class BookVO extends Criteria {
     private int fprice;            // 정가
     private int dprice;            // 할인가
     private int stock;             // 재고 수량
-    private String des;    // 책 소개 (CLOB → String으로 매핑됨)
+    private String des;   		   // 책 소개 (CLOB → String으로 매핑됨)
     private int bcid;              // 카테고리 ID
-    private String fileUrl; // 이미지 다운로드를 위한 URL
-    private Double discount; // 할인율 (메인페이지 할인율 플립도서용)
+    private String fileUrl; 	   // 이미지 다운로드를 위한 URL
+    private Double discount;	   // 할인율 (메인페이지 할인율 플립도서용)
+   
+    private int orderCount;		   // 주문수(메인페이지 베스트셀러에서 활용 승화)
+    	private boolean newFlag;    // 신규 여부(메인페이지 베스트셀러에서 활용 승화)
+    	private int rankUp;         // 순위 상승(메인페이지 베스트셀러에서 활용 승화)
+    	private int rankDown;       // 순위 하락(메인페이지 베스트셀러에서 활용 승화)
 	
 }
